415da9e9ae0acd5f32da30814bcdf29f0ea983af
VivekRanjan001 <90482266+VivekRanjan001@users.noreply.github.com>
2021-09-27 03:35:55 +0530

    Add files via upload

---
+++ Day1.py
@@ -0,0 +1,20 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+
+# Read and save an integer, double, and String to your variables.
+
+input_int = int(input())
+input_double = float(input())
+input_string = str(input())
+
+print(i + input_int)
+
+print(round(d + input_double, 1))
+
+
+print(s + input_string)
+
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
