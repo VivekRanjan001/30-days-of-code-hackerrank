c11ff24031d4e7697764613e3803e8eb4266eda7
VivekRanjan001 <90482266+VivekRanjan001@users.noreply.github.com>
2021-09-28 02:45:14 +0530

    Add files via upload

---
+++ Day21.py
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+/**
+*    Name: printArray
+*    Print each element of the generic vector on a new line. Do not return anything.
+*    @param A generic vector
+**/
+
+// Write your code here
+
+int main() {
