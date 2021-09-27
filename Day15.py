1972961038fb8462be4b5365aee618d65954dbfb
VivekRanjan001 <90482266+VivekRanjan001@users.noreply.github.com>
2021-09-27 05:37:01 +0530

    Add files via upload

---
+++ Day15.py
@@ -0,0 +1,23 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None 
+class Solution: 
+    def display(self,head):
+        current = head
+        while current:
+            print(current.data,end=' ')
+            current = current.next
+
+    def insert(self,head,data):
+        if head is None:
+            head = Node(data)
+        else:
+            current = head
+            while current.next is not None:
+                current = current.next
+            current.next = Node(data)
+        return head
+    #Complete this method
+
+mylist= Solution()
